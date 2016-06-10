4344255cbe57c9f333a20a54a94dbf4b03b05305
melodyJie <735905982@qq.com>
2016-06-10 19:00:34 +0800

    登录注册页面
    登录注册页面写好了,记得尝试每一个按钮,有动画..欢迎大家帮我找BUG

---
+++ 花瓣网/js/someEffect.js
@@ -0,0 +1,111 @@
+window.addEventListener('load',function(){
+	
+	var head_page_box=document.getElementById("head_page_box");
+	var returnTop=document.getElementById("returnTop");
+	window.addEventListener("scroll",function(){
+		//滑动时 背景图的移动
+		var sTop=document.body.scrollTop||document.documentElement.scrollTop;
+
+		if(sTop>=0&&sTop<=500){
+			head_page_box.style.backgroundPosition="center "+sTop/2+"px";
+			returnTop.style.display="none";
+		}
+
+		//返回顶部按钮的实现
+		if(sTop>500){
+			returnTop.style.display="block";
+		}
+	});
+	returnTop.addEventListener('click',function(){
+		var fscroll=document.body.scrollTop||document.documentElement.scrollTop;
+		var time=200;
+		var flash=10;
+		var times=parseInt(time/flash);
+		var nowtimes=times;
+
+		var scrollchange=setInterval(function(){
+			var st=document.body.scrollTop||document.documentElement.scrollTop;
+			if(st>0){
+				if(document.body.scrollTop){
+					document.body.scrollTop=fscroll*nowtimes/times;
+				}else if(document.documentElement.scrollTop){
+					document.documentElement.scrollTop=fscroll*nowtimes/times;
+				}
+				nowtimes--;
+			}else{
+				clearInterval(scrollchange);
+			}
+		},flash);
+	},false);
+	
+	/*登录注册按钮添加事件*/
+	var register_btn=document.getElementById("register_btn");
+	var login_btn=document.getElementById("login_btn");
+
+	var login_close=document.getElementById("login_close");
+	var ls_box=document.getElementById("ls_box");
+
+	var login_frame=document.getElementById("login_frame");
+	var forgetPassword=document.getElementById("forgetPassword");
+	var toSignup=document.getElementById("toSignup");
+	var toLogin=document.getElementById("toLogin");
+	var login_page=document.getElementById("login_page");
+	var signup_page=document.getElementById("signup_page");
+
+
+	register_btn.addEventListener("click",function(){
+		ls_box.style.display="block";
+		signup_page.style.display="block";
+	},false);
+	login_btn.addEventListener("click",function(){
+		ls_box.style.display="block";
+		login_page.style.display="block";
+	},false);
+
+	/*登录注册页面关闭按钮的事件*/
+	
+	login_close.addEventListener("click",function(){
+		ls_box.style.display="none";
+		signup_page.style.display="none";
+		login_page.style.display="none";
+	},false);
+	/*登录注册页面的旋转效果*/
+	
+
+	forgetPassword.addEventListener("click",function(){
+		alert(1);
+	},false);
+
+
+	toSignup.addEventListener("click",function(){
+		login_frame.className+=" switching1";
+		login_frame.addEventListener("webkitAnimationEnd",funToSi1,false);
+	},false);
+	function funToSi1(){
+		login_page.style.display="none";
+		signup_page.style.display="block";
+		login_frame.className=login_frame.className.replace(" switching1"," switching2");
+		login_frame.removeEventListener("webkitAnimationEnd",funToSi1,false);
+		login_frame.addEventListener("webkitAnimationEnd",funToSi2,false);
+	}
+	function funToSi2(){
+		login_frame.className=login_frame.className.replace(" switching2","");
+		login_frame.removeEventListener("webkitAnimationEnd",funToSi2,false);
+	}
+
+	toLogin.addEventListener("click",function(){
+		login_frame.className+=" switching1";
+		login_frame.addEventListener("webkitAnimationEnd",funToLo1,false);
+	},false);
+	function funToLo1(){
+		signup_page.style.display="none";
+		login_page.style.display="block";
+		login_frame.className=login_frame.className.replace(" switching1"," switching2");
+		login_frame.removeEventListener("webkitAnimationEnd",funToLo1,false);
+		login_frame.addEventListener("webkitAnimationEnd",funToLo2,false);
+	}
+	function funToLo2(){
+		login_frame.className=login_frame.className.replace(" switching2","");
+		login_frame.removeEventListener("webkitAnimationEnd",funToLo2,false);
+	}
+})
--- 花瓣网/js/waterFall.js
+++ 花瓣网/js/waterFall.js
@@ -5,11 +5,11 @@ wid:每列的宽度
 addHeight:当滚动条高度加页面高度大于这个值时,需要加入新的图片了*/
 
 
-window.addEventListener('load',function() {
+window.addEventListener('load', function() {
     var positionMessage = {};
     var num = 0;
     var maxNum = 97;
-    waterfall('img_page', 'box',230);
+    waterfall('img_page', 'box', 230);
 
     //换一种写法...
     //已知共存了97张图片,已经加入了20张
@@ -17,63 +17,65 @@ window.addEventListener('load',function() {
     var main = document.getElementById("img_page");
     window.addEventListener('scroll', function() {
         if (checkHeight()) {
-            putNum(main,1);
+            putNum(main, 1);
         }
-    },false);
+    }, false);
     //加载10张图片
-    function putNum(main,onceNum){
+    function putNum(main, onceNum) {
         //可以一次加入一堆,也可以依次加入一个,这里随便怎么写
-            let pd = document.createDocumentFragment();
-            for (var i = 0; i < onceNum; i++) {
-                let box = document.createElement("div");
+        var pd = document.createDocumentFragment();
+        for (var i = 0; i < onceNum; i++) {
+            (function() {
+                var box = document.createElement("div");
                 box.className = "box viewin";
                 pd.appendChild(box);
-                let pic = document.createElement("div");
+                var pic = document.createElement("div");
                 pic.className = "pic";
                 box.appendChild(pic);
 
-                let a=document.createElement("a");
+                var a = document.createElement("a");
                 pic.appendChild(a);
 
-                let img = document.createElement("img");
+                var img = document.createElement("img");
                 img.src = "images/" + num + ".jpg";
                 a.appendChild(img);
 
-                let img_infor_p=document.createElement("p");
-                img_infor_p.className="img_infor";
-                img_infor_p.innerHTML="这里是图片介绍这里是图片介绍这里是图片介绍这里是图片介绍这里是图片介绍这里是图片介绍这里是图片介绍"
+                var img_infor_p = document.createElement("p");
+                img_infor_p.className = "img_infor";
+                img_infor_p.innerHTML = "这里是图片介绍这里是图片介绍这里是图片介绍这里是图片介绍这里是图片介绍这里是图片介绍这里是图片介绍"
                 pic.appendChild(img_infor_p);
 
-                let author_infor_div=document.createElement("div");
-                author_infor_div.className="author_infor";
+                var author_infor_div = document.createElement("div");
+                author_infor_div.className = "author_infor";
                 pic.appendChild(author_infor_div);
 
-                let author_head_a=document.createElement("a");
-                author_head_a.className="author_head";
+                var author_head_a = document.createElement("a");
+                author_head_a.className = "author_head";
                 author_infor_div.appendChild(author_head_a);
 
-                let author_name_p=document.createElement("p");
-                author_name_p.className="author_name";
-                author_name_p.innerHTML="作者名称";
+                var author_name_p = document.createElement("p");
+                author_name_p.className = "author_name";
+                author_name_p.innerHTML = "作者名称";
                 author_infor_div.appendChild(author_name_p)
 
-                let img2=document.createElement("img");
-                img2.src="img_test/author_test.jpg";
+                var img2 = document.createElement("img");
+                img2.src = "img_test/author_test.jpg";
                 author_head_a.appendChild(img2);
 
                 /*调试用*/
                 num++;
                 if (num == maxNum + 1) { num = 0; }
 
-                
+
                 img.onload = function() {
-                    putInMain(box,main);
+                    putInMain(box, main);
                 }
-            }
-            main.appendChild(pd)
+            })();
+        }
+        main.appendChild(pd)
     }
 
-    function waterfall(parent, classname,wid) {
+    function waterfall(parent, classname, wid) {
         var main = document.getElementById(parent);
         var mWidth = main.offsetWidth;
         //num为列数
@@ -87,20 +89,20 @@ window.addEventListener('load',function() {
         //offsetTop 的bug
         positionMessage.top = main.offsetTop;
         //解决bug   学到了:getComputedStyle(node) 与 currentStyle
-        
-        if(window.getComputedStyle){
-            for(let node=main;node.parentNode!=document.body;node=node.parentNode){
+
+        if (window.getComputedStyle) {
+            for (var node = main; node.parentNode != document.body; node = node.parentNode) {
                 //console.log(node.parentNode.nodeName);
-                if(getComputedStyle(node.parentNode).position!="static"){
-                    positionMessage.top +=node.parentNode.offsetTop;
+                if (getComputedStyle(node.parentNode).position != "static") {
+                    positionMessage.top += node.parentNode.offsetTop;
                 }
-            }    
-        }else{
-            for(let node=main;node.parentNode!=document.body;node=node.parentNode){
-                if(node.parentNode.currentStyle.position==="static"){
-                    positionMessage.top +=node.parentNode.offsetTop;
+            }
+        } else {
+            for (var node = main; node.parentNode != document.body; node = node.parentNode) {
+                if (node.parentNode.currentStyle.position === "static") {
+                    positionMessage.top += node.parentNode.offsetTop;
                 }
-            } 
+            }
         }
 
         for (var i = 0; i < positionMessage.length; i++) {
@@ -113,10 +115,10 @@ window.addEventListener('load',function() {
         main.style.margin = "0 auto";
         //main.style.cssText="width:"+wid*num+"px;margin:0 auto;";
 
-        putNum(main,20);
+        putNum(main, 20);
     }
     //将生成的box放在该有的位置(通过positionMessage获得) 并更新positionMessage
-    function putInMain(element,parent) {
+    function putInMain(element, parent) {
         var minHei = Math.min.apply(null, positionMessage.hei);
         //console.log(minHei);
         var index = findIndex(minHei);
@@ -130,10 +132,10 @@ window.addEventListener('load',function() {
         //console.log(i+'所加位置的高度:'+positionMessage.addHeight);
         positionMessage.hei[index] += element.offsetHeight;
 
-        parent.style.height = (Math.max.apply(null,positionMessage.hei)+250)+'px'
-        //console.log("添加的元素高度:"+element.offsetHeight);
-        //console.log(i+'加完之后当列的高度:'+positionMessage.hei[index]	);
-        //console.log(positionMessage.hei);
+        parent.style.height = (Math.max.apply(null, positionMessage.hei) + 250) + 'px'
+            //console.log("添加的元素高度:"+element.offsetHeight);
+            //console.log(i+'加完之后当列的高度:'+positionMessage.hei[index]	);
+            //console.log(positionMessage.hei);
     }
     //寻找高度最小的列 的索引值
     function findIndex(min) {
@@ -146,6 +148,6 @@ window.addEventListener('load',function() {
         //滚动条的高度
         var scrollHei = document.body.scrollTop || document.documentElement.scrollTop;
         var clientHei = document.documentElement.clientHeight || document.body.clientHeight;
-        return (positionMessage.addHeight+positionMessage.top+250 < scrollHei + clientHei) ? true : false;
+        return (positionMessage.addHeight + positionMessage.top + 250 < scrollHei + clientHei) ? true : false;
     }
-},false);
+}, false);
